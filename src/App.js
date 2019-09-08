d1dfd3f5bf5dc3be632af8f33a0d7b6e4390e574
Vachirarat Thavisaengsiri <pae9769@gmail.com>
2019-09-09 00:09:57 +0700

    done

--- src/App.js
+++ src/App.js
@@ -1,35 +1,68 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState } from 'react'
 import './App.css'
 import 'materialize-css/dist/css/materialize.min.css'
 import VirusScan from './component/VirusScan'
 import axios from 'axios'
+import qs from 'querystring'
+import M from 'materialize-css'
 
 const App = () => {
   const [scanMessage, setScanMessage] = useState({})
-
-  useEffect(() => {
-    console.log(`React app api_key = ${process.env.REACT_APP_API_KEY}`)
-  }, [])
+  const [scans, setScans] = useState([])
+  const [loading, setLoading] = useState(false)
 
   const uploadFile = async file => {
-    const res = await axios({
-      url: 'https://www.virustotal.com/vtapi/v2/file/scan',
-      method: 'post',
+    setLoading(true)
+    const url = 'https://www.virustotal.com/vtapi/v2/file/scan'
+    const config = {
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded'
+      },
       params: {
         apikey: process.env.REACT_APP_API_KEY
-      },
-      data: {
-        file: file
       }
-    })
-    setScanMessage(res)
-    console.log(res)
-    console.log(file)
+    }
+    const body = {
+      file: file
+    }
+    const res = await axios.post(url, qs.stringify(body), config)
+    setScanMessage(res.data)
+    console.dir(`From uploadfile:`)
+    console.dir(res.data)
+    reportFile(res.data.md5)
+    setLoading(false)
+  }
+
+  const reportFile = async md5 => {
+    const res = await axios.get(
+      `https://www.virustotal.com/vtapi/v2/file/report?apikey=${process.env.REACT_APP_API_KEY}&resource=${md5}`
+    )
+    console.dir(`from reportfile :`)
+    console.dir(res.data)
+
+    let temp = []
+    for (let key in res.data.scans) {
+      temp.push({
+        name: key,
+        detected: res.data.scans[key].detected,
+        version: res.data.scans[key].version,
+        update: res.data.scans[key].update,
+        result: res.data.scans[key].result
+      })
+    }
+    setScanMessage(res.data)
+    setScans(temp)
+    console.log(temp)
   }
 
   return (
     <div className="App">
-      <VirusScan uploadFile={uploadFile} />
+      <VirusScan
+        uploadFile={uploadFile}
+        scanMessage={scanMessage}
+        scans={scans}
+        loading={loading}
+      />
     </div>
   )
 }
--- src/component/VirusScan.js
+++ src/component/VirusScan.js
@@ -1,10 +1,10 @@
 import React, { useState } from 'react'
 
-const VirusScan = ({ uploadFile }) => {
+const VirusScan = ({ uploadFile, scanMessage, scans, loading }) => {
   const handleSubmit = e => {
     e.preventDefault()
+    if (file === null) return
     console.log(file)
-    // uploadFile(file)
 
     let reader = new FileReader()
     reader.readAsDataURL(file)
@@ -16,21 +16,73 @@ const VirusScan = ({ uploadFile }) => {
   const [file, setFile] = useState(null)
 
   return (
-    <div className="container">
-      <div className="row">
-        <div className="col s12">
-          <form onSubmit={handleSubmit}>
-            <label htmlFor="file"></label>
-            <input
-              type="file"
-              name="file"
-              onChange={e => setFile(e.target.files[0])}
-            />
-            <input type="submit" value="Submit" />
-          </form>
+    <section className="section section-scan">
+      <div className="container">
+        <div className="row">
+          <div className="col s6 offset-s3">
+            <form onSubmit={handleSubmit}>
+              <label htmlFor="file"></label>
+              <div className="file-field input-field">
+                <div className="btn">
+                  <span>File</span>
+                  <input
+                    type="file"
+                    name="file"
+                    onChange={e => setFile(e.target.files[0])}
+                  />
+                </div>
+                <div className="file-path-wrapper">
+                  <input className="file-path validate" type="text" />
+                </div>
+              </div>
+              <input
+                type="submit"
+                value="Submit"
+                className="btn waves-effect waves-light"
+              />
+            </form>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col s12">
+            {loading && (
+              <div className="progress">
+                <div className="indeterminate"></div>
+              </div>
+            )}
+            <p className="red-text">{scanMessage.verbose_msg}</p>
+            <p className="blue-text">{scanMessage.md5}</p>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col s12">
+            <ul>
+              {scans.map(scan => (
+                <li key={scan.name}>
+                  <p>
+                    <span className="green-text">{scan.name}</span> version:{' '}
+                    {scan.version}
+                  </p>
+                  <p>
+                    Detected:{' '}
+                    {scan.detected ? (
+                      <span className="red-text">
+                        <strong>True</strong>
+                      </span>
+                    ) : (
+                      <span className="blue-text">False</span>
+                    )}
+                  </p>
+                  {scan.detected && (
+                    <p className="red-text">Result: {scan.result}</p>
+                  )}
+                </li>
+              ))}
+            </ul>
+          </div>
         </div>
       </div>
-    </div>
+    </section>
   )
 }
 
